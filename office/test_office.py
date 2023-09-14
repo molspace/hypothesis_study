5e1fb220c5989a7c1dbf84f6408764964f4d6001
molspace <nurlybek.amangeldiev@gmail.com>
2023-09-14 19:34:11 +0600

    Added composite team example to decrease boilerplate code on teams generation

--- office/test_office.py
+++ office/test_office.py
@@ -1,23 +1,31 @@
 from typing import Callable
 import pytest
 from hypothesis import given, settings
-from hypothesis.strategies import integers
+from hypothesis.strategies import integers, composite, SearchStrategy
 
 from office import Employee, fire_random_employee, generate_random_team
 
+@composite
+def teams(draw: Callable[[SearchStrategy[int]], int], min_value: int = 1, max_value: int = 20):
+	rand_val = draw(integers(min_value, max_value))
+	return generate_random_team(rand_val)
 
 @given(integers(max_value=0))
 def test_negative_team_size(team_size: int):
 	with pytest.raises(ValueError):
 		generate_random_team(team_size)
 
-@given(integers(min_value=1, max_value=20))
+@given(integers(1,20))
 def test_team_size(team_size: int):
-	assert len(generate_random_team(team_size)) == team_size
-
-@given(integers(min_value=1, max_value=20))
-def test_team_has_ceo(team_size: int):
 	team = generate_random_team(team_size)
+	assert len(team) == team_size
+
+@given(teams())
+def test_team_has_ceo(team):
 	assert Employee.CEO in team
 
-@given(integers(min_value=1, max_value=20))
+@given(teams())
+def test_fire_employee(team):
+	team_copy = team.copy()
+	fire_random_employee(team_copy)
+	assert len(team_copy) == len(team) - 1
